4d6ef38b6a7d07f9e1edf5ec0d2647eef352058d
Dan Webb <dan.webb@damacus.io>
2025-03-17 10:16:58 +0000

    More git fixes
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- dot_config/private_fish/functions/git-delete-local-merged.fish
+++ dot_config/private_fish/functions/git-delete-local-merged.fish
@@ -1,12 +1,12 @@
 function git-delete-local-merged
-    set main_branch (git main-branch)
-    
+    set main_branch (git-main-branch)
+
     colour_log "Removing merged branches..."
     set branches (git branch --merged | grep -v "^\*" | grep -v $main_branch | tr -d '\n')
     if test -n $branches
         echo "$branches" | xargs git branch -d
     end
-    
+
     colour_log "Removing branches squashed or merged into $main_branch..."
     git for-each-ref refs/heads/ --format="%(refname:short)" | while read branch
         set base (git merge-base $main_branch $branch)
